2878016f70923bf9b86716c31092272286741601
Mauro <mauro@osn.global>
2019-08-18 21:46:03 +0200

    Add gzip middleware

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,6 @@ module DaoBrowserApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
